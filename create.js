0b24194077dcf89706be8dacfa30bc8308ecccd5
Towner Hale <townerhaleTowner@Towners-MacBook-Air.local>
2020-09-18 11:30:42 -1000

    added dynamodb-table yml
    This fixed my 500 server error on the client side.

--- create.js
+++ create.js
@@ -2,35 +2,28 @@ import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-//makes our lambda function asynchronous and return the results
 export const main = handler(async (event, context) => {
-    // Request body
-    const data = JSON.parse(event.body);
-    const params =
-    {
-        TableName: process.env.tableName,
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Cognito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from request body
+    // - 'attachment': parsed from request body
+    // - 'createdAt': current Unix timestamp
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
 
-        /*'Item' contains the attributes of the item to be
-        created
-        - 'userId': user identities are federated
-        through the
-        Cognito Identity Pool, we will use
-        the identity id as the user id of the authenticated user
-        - 'noteId': a unique uuid
-        - 'content': parsed from request body
-        - 'attachment': parsed from request body
-        - 'createdAt': current Unix timestamp */
-        Item:
-        {
-            userId:
-            event.requestContext.identity.cognitoIdentityId,
-            noteId: uuid.v1(),
-            content: data.content,
-            attachment: data.attachment,
-            createdAt: Date.now()
-        }
-    };
-    await dynamoDb.put(params);
+  await dynamoDb.put(params);
 
-    return params.Item;
+  return params.Item;
 });
